eff65eeccacef59f9919ee0f3821849cf7710506
David Schneider <david.schneider@bivab.de>
2015-02-02 16:43:38 +0100

    Set the prolog template for wiwi data to the same data.pl.template template as phil-fak

--- src/main/groovy/de/stups/slottool/Faculty.groovy
+++ src/main/groovy/de/stups/slottool/Faculty.groovy
@@ -4,7 +4,7 @@ package de.stups.slottool
  * Created by David Schneider on 01.02.15.
  */
 enum Faculty {
-   WiWi("wiwi", [(FileType.BMachine.name): "wiwi_data.mch.template"]),
+   WiWi("wiwi", [(FileType.BMachine.name): "wiwi_data.mch.template", (FileType.Prolog.name): "data.pl.template"]),
    PhilFak("philfak", [(FileType.BMachine.name): "data.mch.template", (FileType.Prolog.name): "data.pl.template"]);
 
    private final name
