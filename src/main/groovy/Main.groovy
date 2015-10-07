5266a435e94b2a13d9c0244167d9da7e2b3a4bcf
David Schneider <david.schneider@bivab.de>
2015-10-07 11:19:08 +0200

    Bump version: 1.0.0 → 1.0.1-SNAPSHOT

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -5,7 +5,7 @@ import de.stups.slottool.data.Store
 
 class Main {
 
-    static String VERSION = "1.0.0"
+    static String VERSION = "1.0.1-SNAPSHOT"
     public static void main(String[] args) {
         printVersion()
         /*
