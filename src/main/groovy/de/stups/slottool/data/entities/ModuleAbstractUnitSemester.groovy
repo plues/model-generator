b6f1f52481c3e00716945f5b264abcaf14312b7c
David Schneider <david.schneider@bivab.de>
2015-10-09 20:37:06 +0200

    typo

--- src/main/groovy/de/stups/slottool/data/entities/ModuleAbstractUnitSemester.groovy
+++ src/main/groovy/de/stups/slottool/data/entities/ModuleAbstractUnitSemester.groovy
@@ -3,11 +3,11 @@ package de.stups.slottool.data.entities
 class ModuleAbstractUnitSemester {
     Module module
     AbstractUnit abstract_unit
-    Integer semeter
+    Integer semester
 
     def ModuleAbstractUnitSemester(Module module, AbstractUnit au, Integer semester) {
         this.module = module
         this.abstract_unit = au
-        this.semeter = semester
+        this.semester = semester
     }
 }
