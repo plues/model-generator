8c244f593912a64d270e8676565b1f0481bfa498
David Schneider <david.schneider@bivab.de>
2015-10-09 20:36:41 +0200

    Pass kzfa field to Course constructor and added methods to build name and full name strings from the different properties

--- src/main/groovy/de/stups/slottool/data/entities/Course.groovy
+++ src/main/groovy/de/stups/slottool/data/entities/Course.groovy
@@ -2,22 +2,31 @@ package de.stups.slottool.data.entities
 
 class Course {
     int id
-    String name
+    String short_name
     String long_name
     String degree
     int po
     Date created_at
     Date updated_at
     Set<Level> levels
+    String kzfa
 
-    def Course(int id, String name, String short_name, String degree, int po, Date created_at, Date updated_at) {
+    def Course(int id, String long_name, String short_name, String degree, String kzfa, int po, Date created_at, Date updated_at) {
         this.id = id
-        this.name = name
-        this.long_name = short_name
+        this.short_name = short_name
+        this.long_name = long_name
         this.degree = degree
+        this.kzfa = kzfa
         this.po = po
         this.created_at = created_at
         this.updated_at = updated_at
         this.levels = new HashSet<>()
+    }
+
+    def getName() {
+        return "${this.short_name}-${this.degree}-${this.kzfa}-${this.po}"
+    }
+    def getFullName() {
+        return "${this.long_name} (${this.degree} ${this.kzfa}) PO:${this.po}"
     }
 }
