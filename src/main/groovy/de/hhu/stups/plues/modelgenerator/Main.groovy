7931c4d67c0681d71d7885fba9f50e6a8946a469
David Schneider <david.schneider@bivab.de>
2016-07-21 12:53:00 +0200

    Updated command line interface. Templates can now either be a file or a reference to the builtin templates (currently b and xml)

--- src/main/groovy/de/hhu/stups/plues/modelgenerator/Main.groovy
+++ src/main/groovy/de/hhu/stups/plues/modelgenerator/Main.groovy
@@ -2,6 +2,9 @@ package de.hhu.stups.plues.modelgenerator
 
 import de.hhu.stups.plues.data.Store
 
+import java.nio.file.Files
+import java.nio.file.Path
+
 class Main {
 
     static String VERSION = "3.0.0-SNAPSHOT"
@@ -11,7 +14,8 @@ class Main {
         /*
          * http://marxsoftware.blogspot.de/2010/07/explicitly-specifying-args-property.html
          */
-        def cli = new CliBuilder(usage: 'java -jar JARFILE --output=<path> --database=<path> [--template=<path>]\nIf no template is provided the default it to generate a B-Machine (.mch)')
+        def cli = new CliBuilder(usage: 'java -jar JARFILE --output=<path> --database=<path> [--template=<path>]\n' +
+                'If no template is provided, the default is to generate a B-Machine (.mch)')
         cli.with {
             t(longOpt: 'template', "External template to be used for rendering", args: 1)
             o(longOpt: 'output', 'Target file', args: 1, required: true)
@@ -30,12 +34,26 @@ class Main {
         def output = options.output.replaceFirst("^~", System.getProperty("user.home"));
         def result
 
+        FileType ft;
+
         /* If no template is provided we default to generating a B (.mch) file */
-        if (options.template) {
-            result = renderer.renderWith(options.template)
+        if(options.template) {
+            ft = FileType.Unknown;
+            // try to detect if we are requesting a builtin template by name
+            FileType.each { i ->
+                if(i.name == options.template) {
+                    ft = i
+                }
+            }
         } else {
-            def tp = FileType.BMachine
-            result = renderer.renderFor(tp)
+            ft = FileType.BMachine
+        }
+
+        if (ft == FileType.Unknown) {
+            def template = options.template.replaceFirst("^~", System.getProperty("user.home"));
+            result = renderer.renderWith(template)
+        } else {
+            result = renderer.renderFor(ft)
         }
         def fw = new FileWriter(new File(output))
         result.writeTo(fw)
