ceb2627db2e7e2124654bc7f03fe66023664f317
David Schneider <david.schneider@bivab.de>
2016-09-19 23:01:14 +0200

    renamed name fiend in FileType enum

--- src/main/java/de/hhu/stups/plues/modelgenerator/FileType.java
+++ src/main/java/de/hhu/stups/plues/modelgenerator/FileType.java
@@ -3,16 +3,16 @@ package de.hhu.stups.plues.modelgenerator;
 public enum FileType {
   BMachine("b", "mch"), ModuleCombination("mc", "xml"), Unknown("?", "?");
 
-  public final String name;
+  public final String typeName;
   public final String extension;
 
-  FileType(final String name, final String extension) {
-    this.name = name;
+  FileType(final String typeName, final String extension) {
+    this.typeName = typeName;
     this.extension = extension;
   }
 
   @Override
   public String toString() {
-    return this.name;
+    return this.typeName;
   }
 }
