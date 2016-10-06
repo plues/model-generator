5f9b5c422de08de20dde9cd1efdbea6bb8da3596
David Schneider <david.schneider@bivab.de>
2016-10-06 09:54:48 +0200

    Removed unused import

--- src/main/java/de/hhu/stups/plues/data/SqliteStore.java
+++ src/main/java/de/hhu/stups/plues/data/SqliteStore.java
@@ -20,7 +20,6 @@ import org.hibernate.query.Query;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
