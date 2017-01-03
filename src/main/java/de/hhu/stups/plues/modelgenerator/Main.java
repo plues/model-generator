f25d57f11eb4db8a9258b1e48919d61d2b910fca
David Schneider <david.schneider@bivab.de>
2017-01-03 17:11:15 +0100

    Bump version: 4.3.0-SNAPSHOT → 4.3.0

--- src/main/java/de/hhu/stups/plues/modelgenerator/Main.java
+++ src/main/java/de/hhu/stups/plues/modelgenerator/Main.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 
 public class Main {
   private static final String OUTPUT = "output";
-  private static final String VERSION = "4.3.0-SNAPSHOT";
+  private static final String VERSION = "4.3.0";
 
   private Main() {}
 
