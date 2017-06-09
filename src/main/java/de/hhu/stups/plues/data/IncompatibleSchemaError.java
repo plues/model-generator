785996b5c988624ba13c37e0c8a0b8780b428307
David Schneider <david.schneider@bivab.de>
2017-06-09 14:30:34 +0200

    Made class package private

--- src/main/java/de/hhu/stups/plues/data/IncompatibleSchemaError.java
+++ src/main/java/de/hhu/stups/plues/data/IncompatibleSchemaError.java
@@ -3,7 +3,7 @@ package de.hhu.stups.plues.data;
 import net.sf.ehcache.util.FindBugsSuppressWarnings;
 
 @FindBugsSuppressWarnings("RANGE_ARRAY_INDEX")
-public class IncompatibleSchemaError extends Exception {
+class IncompatibleSchemaError extends Exception {
   IncompatibleSchemaError(final String message) {
     super(message);
   }
